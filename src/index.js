e2630813e0b83f144b79027bd627be07ecfcde00
Yohann-mtx <137070655+Yohann-mtx@users.noreply.github.com>
2024-04-09 10:06:20 +0200

    connection fonctionnelle

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import "dotenv/config"
 import express, { response } from "express";
 import ChampionsRoutes from "./routes/champions.js";
-
+import mongoose from 'mongoose';
 
 const app = express();
 
 console.log("env: ", process.env.MONGO_STRING);
 
 const PORT = process.env.PORT || 3001;
+const MONGO_STRING = process.env.MONGO_STRING
 
 /* Middlewares
  Middlewares qui se charge de regiriger les req
@@ -24,7 +25,10 @@ app.get("/", (request, response) => {
     response.json({ message: "Hello World"})
 })
 
-
-app.listen(PORT, () =>{
-    console.log(`Serveur is running on port ${PORT}`);
+mongoose.connect(MONGO_STRING).then(() => {
+    console.log('Connected to the database!');
+  
+    app.listen(PORT, () =>{
+        console.log(`Serveur is running on port ${PORT}`);
+    })
 });
